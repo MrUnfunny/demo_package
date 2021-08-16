3c867e46fbe4b0b10f3e37f517e5c65d218504f0
Mohit Kumar Singh <mrunfunny2599@gmail.com>
2021-08-16 09:40:54 +0530

    initialized

---
+++ example/demo_package_example.dart
@@ -0,0 +1,6 @@
+import 'package:demo_package/demo_package.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/demo_package_test.dart
@@ -0,0 +1,16 @@
+import 'package:demo_package/demo_package.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
