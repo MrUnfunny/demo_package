d08185037784a78b86e1a831c1cc8256b3381e72
Mohit Kumar Singh <mrunfunny2599@gmail.com>
2021-08-19 13:04:06 +0530

    fix: updated

--- test/demo_package_test.dart
+++ test/demo_package_test.dart
@@ -5,7 +5,7 @@ void main() {
   group('A group of tests', () {
     final awesome = Awesome();
 
-    test('Sure Test', () {
+    test('final Test', () {
       expect(awesome.isAwesome, isTrue);
     });
   });
